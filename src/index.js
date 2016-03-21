0d82a6112713a5753fddccdb9ed1326215155803
Alex Chepura <a.chepura@gmail.com>
2016-03-21 14:54:48 +0300

    cross-env

--- src/index.js
+++ src/index.js
@@ -1,21 +1,7 @@
 "use strict"
 import Redaxtor from './Redaxtor'
 import Piece from './components/Piece'
-// require("./redaxtor.less");
-
-// export {
-//     createStore,
-//     combineReducers,
-//     bindActionCreators,
-//     applyMiddleware,
-//     compose
-// }
-
-// export Piece = Piece;
-// export default Redaxtor;
-// export { Redaxtor as default , Piece};
-
-// impor {Piece} from '.'
+require("./redaxtor.less");
 
 Redaxtor.Piece = Piece;
 module.exports = Redaxtor
