b6d2ba49a1beaab089264f636293d3b57b8bf63e
AndrewK <andrew@spiralscout.com>
2016-12-15 16:35:04 +0300

    More consistent styles

--- src/components/PanelHandler.js
+++ src/components/PanelHandler.js
@@ -5,7 +5,7 @@ class PanelHandler extends Component {
     render() {
         return (
             <div className="r_bar-header" onMouseDown={this.props.onMouseDown} onClick={this.props.toggleOpen}>
-                <span>Redaxtor</span>
+                <span>RedaXtor</span>
                 <button className="r_bar-header-button">
                     {this.props.isOpen ? <i className="r_icon-down-dir"></i> : <i className="r_icon-up-dir"></i>}
                 </button>
