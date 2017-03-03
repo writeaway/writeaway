c7a2155923e17b30054656897a0bc658a93d0bed
AndrewK <andrew@spiralscout.com>
2017-03-03 20:30:27 +0300

    But back source editor

--- src/reducers/pieces.js
+++ src/reducers/pieces.js
@@ -82,7 +82,9 @@ const pieces = (pieces = piecesDefault, action) => {
             } else {
                 return {...pieces, editorActive: false};
             }
-
+            
+        case C.PIECES_SET_SOURCE_ID:
+            return {...pieces, sourceId: action.id};
 
         case C.PIECE_ADD:
             return {
