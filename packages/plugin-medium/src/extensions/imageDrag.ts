796b21a6ba809291ef32646c48ce3df2303a5b55
Andrew Kirkovski <andrew@spiralscout.com>
2020-10-06 17:45:22 +0300

    migrating medium plugins

--- packages/plugin-medium/src/extensions/imageDrag.ts
+++ packages/plugin-medium/src/extensions/imageDrag.ts
@@ -1,30 +1,31 @@
-var MediumEditor = require('medium-editor/dist/js/medium-editor.js');
-(function () {
-    'use strict';
+import MediumEditor from 'medium-editor';
 
-    var ImageDrag = MediumEditor.Extension.extend({
-        init: function () {
-            MediumEditor.Extension.prototype.init.apply(this, arguments);
-            this.subscribe('editableDrag', this.handleDrag.bind(this));
-            this.subscribe('editableDrop', this.handleDrop.bind(this));
-            this.base.origElements.addEventListener('dragstart', this.handleDragStart.bind(this));
-        },
-        handleDragStart: function (e) {
-            (e.target.tagName && e.target.tagName.toLocaleLowerCase() === 'img') ? this.img = true : e.preventDefault();
-        },
-        handleDrag: function (event) {
-            var className = 'medium-editor-dragover';
-            if (event.type === 'dragover') {
-                event.target.classList.add(className);
-            } else if (event.type === 'dragleave' && event.target.classList) {
-                event.target.classList.remove(className);
-            }
-        },
-        handleDrop: function (event) {
-            var className = 'medium-editor-dragover';
-            event.target.classList.remove(className);
-            setTimeout(function(){},100)
-        }
-    });
-    MediumEditor.extensions.imageDrag = ImageDrag;
-}());
+export const ImageDrag = (MediumEditor as any).Extension.extend({
+  init: function () {
+    (MediumEditor as any).Extension.prototype.init.apply(this, arguments);
+    this.subscribe('editableDrag', this.handleDrag.bind(this));
+    this.subscribe('editableDrop', this.handleDrop.bind(this));
+    this.base.origElements.addEventListener('dragstart', this.handleDragStart.bind(this));
+  },
+  handleDragStart: function (e: DragEvent) {
+    const target = e.target as HTMLElement | null;
+    (target?.tagName && target.tagName.toLocaleLowerCase() === 'img') ? this.img = true : e.preventDefault();
+  },
+  handleDrag: function (event: DragEvent) {
+    const target = event.target as HTMLElement | null;
+    const className = 'medium-editor-dragover';
+    if (event.type === 'dragover') {
+      target?.classList.add(className);
+    } else if (event.type === 'dragleave' && target?.classList) {
+      target?.classList.remove(className);
+    }
+  },
+  handleDrop: function (event: DragEvent) {
+    const target = event.target as HTMLElement | null;
+    const className = 'medium-editor-dragover';
+    target?.classList.remove(className);
+    setTimeout(function () {
+    }, 100)
+  }
+});
+(MediumEditor as any).extensions.imageDrag = ImageDrag;
